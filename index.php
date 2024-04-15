ae4952f6a29585745d760f9adac3f4534368b42c
Possible <ajigbotoshojoshua2000@gmail.com>
2024-04-15 13:30:33 +0100

    post loops

--- index.php
+++ index.php
@@ -17,13 +17,46 @@
         if (have_posts()) {
             ?>
             <div class="container">
-                 <?php 
+              <?php
+                if ( is_home() && ! is_front_page() ) {
+                  ?>
+                  <header class="mb-5">
+                      <h1 class="page-title ">
+                        <?php single_post_title(); ?>
+                      </h1>
+                </header>
+                  <?php
+                }
+               ?>
+                <div class="row">
+                   <?php 
+                   $index = 0;
+                   $no_of_columns = 3;
+
+                   //start the loop
                     while (have_posts()) : the_post();
-                    the_title();
-                    the_content();
+                      if (0 === $index % $no_of_columns) {
+                        ?>
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+                        <?php 
+                      }
+                         ?>
+                         <h3><?php the_title();  ?></h3>
+                         <div><?php the_excerpt();  ?></div>
+                        <?php
+
+                      $index++;
+
+                      if (0 !== $index && 0 === $index % $no_of_columns) {
+                        ?>
+                          </div>
+                        <?php 
+                      }
+                   
                     endwhile;
                 ?>
-            </div>
+                   </div>
+                </div>
             <?php
         }
         ?>
